9bbfea533a36f5da715f8bf824164c70ec417334
alterlleo <leonardo.pasquato@gmail.com>
2026-04-27 15:03:35 +0200

    fix: steppers acceleration

--- src/machine.hpp
+++ src/machine.hpp
@@ -135,7 +135,7 @@ namespace cncpp{
 
       string _settings_file = "";
       data_t _A = 5.0;
-      data_t _A_stepper = 0.5;
+      data_t _A_stepper = 2.5;
       Point _zero = Point(0, 0, 0, 0, 0);
       Point _offset = Point(0, 0, 0, 0, 0);
       Point _setpoint, _position;         // setpoint is the point given to the machine
